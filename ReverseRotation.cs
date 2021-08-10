4f0f774b889108e05c52a426a7829381634fb9b3
Papercut110 <toroid87@mail.ru>
2021-08-10 20:32:50 +0300

    upd

---
+++ ReverseRotation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificCollections
+{
+    public class ReverseRotation<T>
+    {
+        int position = -1;
+
+        private T[] _values;
+        int _size;
+        int _pos;
+
+        public ReverseRotation(int size)
+        {
+            _size = size;
+            _values = new T[size];
+        }
+
+        public void Add(T item)
+        {
+            if (_pos < _size)
+            {
+                _values[_pos] = item;
+                _pos++;
+            }
+            else if (_pos == _size)
+            {
+                var regrouped = new T[_size];
+                Array.ConstrainedCopy(_values, 0, regrouped, 1, _size - 1);
+                regrouped[0] = item;
+                _values = regrouped;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < _values.Length - 1)
+                {
+                    position++;
+                    yield return _values[position];
+                }
+                else
+                {
+                    Reset();
+                    yield break;
+                }
+            }
+        }
+
+        private void Reset()
+        {
+            position = -1;
+        }
+
+    }
+}
---
+++ Rotation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Buffers;
+using System.Collections;
+using System.Linq;
+
+namespace SpecificCollections
+{
+    public class Rotation<T>
+    {
+        int position = -1;
+
+        private T[] _values;
+        int _size;
+        int _pos;
+
+        public Rotation(int size)
+        {
+            _size = size;
+            _values = new T[size];
+        }
+
+        public void Add(T item)
+        {
+            if (_pos < _size)
+            {
+                _values[_pos] = item;
+                _pos++;
+            }
+            else if (_pos == _size)
+            {
+                var regrouped = new T[_size];
+                Array.ConstrainedCopy(_values, 1, regrouped, 0, _size - 1);
+                regrouped[regrouped.Length - 1] = item;
+                _values = regrouped;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < _values.Length - 1)
+                {
+                    position++;
+                    yield return _values[position];
+                }
+                else
+                {
+                    Reset();
+                    yield break;
+                }
+            }
+        }
+
+        private void Reset()
+        {
+            position = -1;
+        }
+    }
+}
